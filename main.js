d88a16a100bd11ec3be7b4153ce70fd3ec8f3eec
Neha-2 <nehadeshpande97@gmail.com>
2022-12-11 23:13:17 +0100

    comments added

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 //function for getting competitors using an api
 function get_comp(){
-    location.replace("file:///C:/Users/nehad/Downloads/figma_htmlgenerator/view_competitors.html")
+    
 }
 
 //function for entering hypothesis 
 function test_hypo(){
-    location.replace("file:///C:/Users/nehad/Downloads/figma_htmlgenerator/test_hypo.html")
+    
 }
 
 //function for getting measurements
 function get_measure(){
-    location.replace("file:///C:/Users/nehad/Downloads/figma_htmlgenerator/view_measure.html")
+    
 }
 
 //function for getting insights
 function insights(){
-    location.replace("file:///C:/Users/nehad/Downloads/figma_htmlgenerator/insights.html")
+    
 }
