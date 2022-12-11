60a48e597daed2ee9ebdfbbee2546bf47d9c5212
Neha-2 <nehadeshpande97@gmail.com>
2022-12-11 23:18:18 +0100

    comments updated

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 //function for getting competitors using an api
 function get_comp(){
-    
+    // competitors can be acquired by a keyword search algorithm that helps to match keywords 
+    // with others websites 
+    // the business that has the most matching keywords is the competitor
 }
 
 //function for entering hypothesis 
@@ -10,10 +12,11 @@ function test_hypo(){
 
 //function for getting measurements
 function get_measure(){
-    
+    // this can be done by considering the hypothesis and the measure entered by the user 
 }
 
 //function for getting insights
 function insights(){
-    
+    // this provides a detailed report of the different types of competitors and some 
+    // strategies that proved to be successful for them for a certain measure
 }
